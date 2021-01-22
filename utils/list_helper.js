943bcc86ed3312da6a86c0cfc222c850e5c74dc2
shotius <sh.archemashvili@gmail.com>
2021-01-22 21:09:50 +0400

    4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,5 +1,16 @@
 const dummy = (blogs) => {
-    return 1
+  return 1
 }
 
-module.exports = {dummy}
+const totalLikes = (blogs) => {
+  const reducer = (sum, item) => sum + item.likes
+
+  return blogs.length === 0
+    ? 0
+    : blogs.reduce(reducer, 0)
+}
+
+module.exports = {
+  dummy,
+  totalLikes
+}
