5417d4ffad28f7abecaceedbb163526887d5be8d
shotius <sh.archemashvili@gmail.com>
2021-01-25 13:59:36 +0400

    4.13 4.14

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -28,6 +28,17 @@ blogsRouter.post('/', async (request, response) => {
 
 })
 
+blogsRouter.put('/:id', async (request, response) => {
+  const body = request.body
+
+  const blog = {
+    likes: body.likes
+  }
+
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true} )
+  response.json(updatedBlog.toJSON())
+})
+
 blogsRouter.delete('/:id', async (request, response) => {
   await Blog.findByIdAndRemove(request.params.id)
   response.status(204).end()
--- tests/blogs_api.test.js
+++ tests/blogs_api.test.js
@@ -14,66 +14,74 @@ beforeEach(async () => {
   }
 })
 
-test('returns all blogs', async () => {
-  const blogs = await api.get('/api/blogs')
-
-  expect(blogs.body).toHaveLength(helper.initialBlogs.length)
-})
-
-test('unique identifier is "id" not "_id"', async () => {
-  const blogs = await api.get('/api/blogs')
-
-    
-  blogs.body.forEach((blog) => {
-    expect(blog.id).toBeDefined()
+// getting blogs 
+describe('when there are some blogs saved', () => {
+  test('returns all blogs', async () => {
+    const blogs = await api.get('/api/blogs')
+  
+    expect(blogs.body).toHaveLength(helper.initialBlogs.length)
+  })
+  
+  test('unique identifier is "id" not "_id"', async () => {
+    const blogs = await api.get('/api/blogs')
+  
+      
+    blogs.body.forEach((blog) => {
+      expect(blog.id).toBeDefined()
+    })
   })
 })
 
-test('blog post is added successfully by post methoed', async () => {
-  const newBlog = {
-    title: 'test', 
-    author: 'test', 
-    url: 'http://test.com', 
-    likes: 0 
-  }
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect('Content-Type', /application\/json/)
-
-  const blogsAtEnd = await helper.blogsInDb()
-
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+// adding blogs
+describe('addition of new blog', () => {
+  test('blog post is added successfully by post methoed', async () => {
+    const newBlog = {
+      title: 'test', 
+      author: 'test', 
+      url: 'http://test.com', 
+      likes: 0 
+    }
+  
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect('Content-Type', /application\/json/)
+  
+    const blogsAtEnd = await helper.blogsInDb()
+  
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+  })
+  
+  test('if likes are missing will be set to zero', async () => {
+    const newBlog = {
+      title: 'test', 
+      author: 'test', 
+      url: 'http://test.com'
+    }
+  
+    const res = await api
+      .post('/api/blogs')
+      .send(newBlog)
+  
+    expect(res.body.likes).toBe(0)
+  })
+  
+  test('fail 400 if title and url properties are missing', async () => {
+    const newBlog = {
+      author: 'test'
+    }
+  
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+  })
 })
 
-test('likes property is missing from the request, it will default to the value 0', async () => {
-  const newBlog = {
-    title: 'test', 
-    author: 'test', 
-    url: 'http://test.com'
-  }
-
-  const res = await api
-    .post('/api/blogs')
-    .send(newBlog)
-
-  expect(res.body.likes).toBe(0)
-})
-
-test('if title and url properties are missing in the post request return 400', async () => {
-  const newBlog = {
-    author: 'test'
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
-})
 
 // delete specific note
-describe('delete specific note', () => {
+describe('make action on saved notes', () => {
   test('delete specifig blog', async () => {
     const notesAtStart = await helper.blogsInDb()
     
@@ -85,6 +93,21 @@ describe('delete specific note', () => {
   
     const notesAtEnd = await helper.blogsInDb()
     expect(notesAtEnd).toHaveLength(notesAtStart.length - 1) 
+  })
+
+  test('likes will be updated in blog post', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToUpdate = blogsAtStart[0]
+
+    const newLikes = {
+      likes: 999
+    }
+    
+    const updatedBlog = await api
+      .put(`/api/blogs/${blogToUpdate.id}`)
+      .send(newLikes)
+    
+    expect(updatedBlog.body.likes).toBe(newLikes.likes)
   })
 })
 
